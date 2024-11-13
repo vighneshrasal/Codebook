61e8b19a84d22f2f8505a6d16f826f8489536613
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-11-13 15:53:59 +0530

    Update filterReducers.js

--- src/reducers/filterReducers.js
+++ src/reducers/filterReducers.js
@@ -1,25 +1,31 @@
 export const filterReducer = (state, action) => {
-    const {type, payload} = action;
-    
+    const { type, payload } = action;
+
     switch (type) {
         case "PRODUCT_LIST":
-            return {productList: payload.products}
-        
+            return { productList: payload.products }
+
         case "SORT_BY":
-            return
+            return { ...state, sortBy: payload.sortBy }
 
         case "RATINGS":
-            return
+            return { ...state, ratings: payload.ratings }
 
         case "BEST_SELLER_ONLY":
-            return
+            return { ...state, bestSellerOnly: payload.bestSellerOnly }
 
         case "ONLY_IN_STOCKS":
-            return
-        
+            return { ...state, onlyInStock: payload.onlyInStock }
+
         case "CLEAR_FILTER":
-            return
-        
+            return {
+                ...state,
+                onlyInStock: false,
+                bestSellerOnly: false,
+                sortBy: null,
+                ratings: null
+            }
+
         default:
             throw new Error("No Case Found");
     }
