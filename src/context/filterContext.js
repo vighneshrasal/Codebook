6c55fc41fe578fd18d44f2f36c37ee96303e2c2a
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-11-13 15:52:42 +0530

    Update filterContext.js

--- src/context/filterContext.js
+++ src/context/filterContext.js
@@ -24,9 +24,49 @@ export const FilterProvider = ({children}) => {
             }
         });
     }
+
+    function bestSeller(products){
+        return state.bestSellerOnly ? products.filter(product => product.best_seller === true) : products;
+    }
+
+    function inStock(products){
+        return state.onlyInStock ? products.filter(product => product.in_stock === true) : products;
+    }
+
+    function sort(products){
+        if(state.sortBy === "lowtohigh"){
+            return products.sort((a, b) => Number(a.price) - Number(b.price));
+        }
+
+        if(state.sortBy === "hightolow"){
+            return products.sort((a, b) => Number(b.price) - Number(a.price));
+        }
+
+        return products;
+    }
+
+    function rating(products){
+        if(state.rating === "4STARSABOVE"){
+            return products.filter(product => product.rating >= 4);
+        }
+        if(state.rating === "3STARSABOVE"){
+            return products.filter(product => product.rating >= 3);
+        }
+        if(state.rating === "2STARSABOVE"){
+            return products.filter(product => product.rating >= 2);
+        }
+        if(state.rating === "1STARSABOVE"){
+            return products.filter(product => product.rating >= 1);
+        }
+        return products;
+    }
+
+    const filteredProductList = rating(sort(inStock(bestSeller(state.productList))));
  
     const value = {
-        products: state.productList,
+        state,
+        dispatch,
+        products: filteredProductList,
         initialProductList
     }
     return(
