02e6fc71f4c37b58f8bcf8e298efe9fedce344ff
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-11-14 11:23:22 +0530

    Update AllRoutes.js

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom"
-import {HomePage, ProductDetail, ProductsList} from "../pages";
+import {HomePage, Login, ProductDetail, ProductsList, Register} from "../pages";
+
 export const AllRoutes = () => {
   return (
     <>
@@ -7,6 +8,9 @@ export const AllRoutes = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/products" element={<ProductsList />} />
             <Route path="/products/:id" element={<ProductDetail />} />
+
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
         </Routes>
     </>
   )
