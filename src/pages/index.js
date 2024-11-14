7fb6c56bf0a6520d09cca40dd1978ecfb7ecd694
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-11-14 11:23:01 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 export {HomePage} from "./Home/HomePage";
 export {ProductsList} from "./Products/ProductsList";
 export {ProductDetail} from "./ProductDetails";
+
+export {Login} from "./Login";
+export {Register} from "./Register";
