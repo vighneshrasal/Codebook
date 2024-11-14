c227b43a64d09f6d1fbdd57004bee6b3b83af983
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-11-14 10:37:31 +0530

    Update DropdownLoggedIn.js

--- src/components/Elements/DropdownLoggedIn.js
+++ src/components/Elements/DropdownLoggedIn.js
@@ -1,15 +1,17 @@
+import {Link} from "react-router-dom";
+
 export const DropdownLoggedIn = () => {
   return (
     <div id="dropdownAvatar" className="select-none	absolute top-10 right-0 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
         <div className="py-3 px-4 text-sm text-gray-900 dark:text-white">
-            <div className="font-medium truncate">shubham@example.com</div>
+            <div className="font-medium truncate">vighnesh@example.com</div>
         </div>
         <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownUserAvatarButton">
             <li>
-                <a href="/" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">All eBooks</a>
+                <Link to="/products" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">All eBooks</Link>
             </li>
             <li>
-                <a href="/" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                <Link to="/dashboard" className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</Link>
             </li>
         </ul>
         <div className="py-1">
