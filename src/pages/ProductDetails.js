e58d0a35186c441deb551ecbc4ab7c715b687f55
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-11-29 17:17:59 +0530

    Update ProductDetails.js

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -21,12 +21,12 @@ export const ProductDetail = () => {
   useEffect(() => {
     const productInCart = cartList.find(item => item.id === product.id);
 
-    if(productInCart){
-        setInCart(true);
-    }else{
-        setInCart(false);
+    if (productInCart) {
+      setInCart(true);
+    } else {
+      setInCart(false);
     }
-}, [cartList, product.id])
+  }, [cartList, product.id])
 
   return (
     <main>
@@ -54,10 +54,10 @@ export const ProductDetail = () => {
               <span className="font-semibold text-blue-500 border bg-slate-100 rounded-lg px-3 py-1 mr-2">{product.size} MB</span>
             </p>
             <p className="my-3">
-              {!inCart && <button onClick={() => addToCart(product)} className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800`}>Add To Cart <i className="ml-1 bi bi-plus-lg"></i></button>
-            }
-              {inCart && <button onClick={() => removeFromCart(product)} className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800`}  disabled={ product.in_stock ? "" : "disabled" }>Remove Item <i className="ml-1 bi bi-trash3"></i></button>
-            }
+              {!inCart && <button onClick={() => addToCart(product)} className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 ${product.in_stock ? "" : "cursor-not-allowed"}`} disabled={product.in_stock ? "" : "disabled"}>Add To Cart <i className="ml-1 bi bi-plus-lg"></i></button>
+              }
+              {inCart && <button onClick={() => removeFromCart(product)} className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 ${product.in_stock ? "" : "cursor-not-allowed"}`} disabled={product.in_stock ? "" : "disabled"}>Remove Item <i className="ml-1 bi bi-trash3"></i></button>
+              }
             </p>
             <p className="text-lg text-gray-900 dark:text-slate-200">
               {product.long_description}
