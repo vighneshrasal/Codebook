b6846a3ebb182c98132efe41d2f1a35e3d06eaef
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-12-02 15:20:01 +0530

    Update Checkout.js

--- src/pages/Cart/components/Checkout.js
+++ src/pages/Cart/components/Checkout.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import { useCart } from "../../../context"
 
 export const Checkout = ({setCheckOut}) => {
     const {total} = useCart();
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        const token = JSON.parse(sessionStorage.getItem("token"));
+        const cbid = JSON.parse(sessionStorage.getItem("cbid")); 
+
+        async function getUser(){
+            const response = await fetch(`http://localhost:8000/600/users/${cbid}`, {
+                method : "GET",
+                headers : {"Content-Type" : "application/json", Authorization : `Bearer ${token}`}
+            });
+            const data = await response.json();
+            setUser(data);
+        }
+        getUser();
+    },[])
 
     return (
       <section>
@@ -22,11 +39,11 @@ export const Checkout = ({setCheckOut}) => {
                       <form className="space-y-6" >
                       <div>
                           <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name:</label>
-                          <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white" value="Shubham Sarda" disabled required="" />
+                          <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white" value={user.name} disabled required="" />
                       </div>
                       <div>
                           <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email:</label>
-                          <input type="text" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white" value="shubham@example.com" disabled required="" />
+                          <input type="text" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white" value={user.email} disabled required="" />
                       </div>
                       <div>
                           <label htmlFor="card" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Card Number:</label>
