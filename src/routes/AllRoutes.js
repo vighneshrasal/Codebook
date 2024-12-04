5be3cbcd8e071c9865bb018faa0f2855d5cb1d58
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-12-04 15:52:56 +0530

    Update AllRoutes.js

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,24 +1,26 @@
 import { Route, Routes } from "react-router-dom"
-import {CartPage, HomePage, Login, OrderPage, ProductDetail, ProductsList, Register} from "../pages";
+import { CartPage, HomePage, Login, OrderPage, PageNotFound, ProductDetail, ProductsList, Register } from "../pages";
 import { ProtectedRoute } from "./ProtectedRoute";
 import { DashboardPage } from "../pages/Dashboard/DashboardPage";
 
 export const AllRoutes = () => {
-  
+
   return (
     <>
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/products" element={<ProductsList />} />
-            <Route path="/products/:id" element={<ProductDetail />} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsList />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
 
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
 
-            <Route path="cart" element={<ProtectedRoute><CartPage /></ProtectedRoute>}/>
-            <Route path="order-summary" element={<ProtectedRoute><OrderPage/></ProtectedRoute>}/>
-            <Route path="dashboard" element={<ProtectedRoute><DashboardPage/></ProtectedRoute>}/>
-        </Routes>
+        <Route path="cart" element={<ProtectedRoute><CartPage /></ProtectedRoute>} />
+        <Route path="order-summary" element={<ProtectedRoute><OrderPage /></ProtectedRoute>} />
+        <Route path="dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   )
 }
