1620929907ed2b66aac530f25cea9b7c93f5c9ff
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-12-04 12:05:24 +0530

    Update DashboardPage.js

--- src/pages/Dashboard/DashboardPage.js
+++ src/pages/Dashboard/DashboardPage.js
@@ -1,21 +1,15 @@
 import { useEffect, useState } from "react";
 import { DashboardCard } from "./components/DashboardCard";
 import { DashboardEmpty } from "./components/DashboardEmpty";
+import { getUserOrders } from "../../services";
 
 export const DashboardPage = () => {
 
   const [orders, setOrders] = useState([]);
-
-  const token = JSON.parse(sessionStorage.getItem("token"));
-  const cbid = JSON.parse(sessionStorage.getItem("cbid"));
     
   useEffect(() => {
     async function fetchOrders() {
-      const response = await fetch(`http://localhost:8000/660/orders?user.id=${cbid}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` }
-    });
-    const data = await response.json();
+      const data = await getUserOrders();
     setOrders(data);
     }
     fetchOrders();
