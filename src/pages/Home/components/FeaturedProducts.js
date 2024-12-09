377930979cd278ad0204362c623f29ddf81d79b0
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-12-09 12:40:35 +0530

    Update FeaturedProducts.js

--- src/pages/Home/components/FeaturedProducts.js
+++ src/pages/Home/components/FeaturedProducts.js
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react"
 import { ProductCard } from "../../../components/";
 import { getFeaturedList } from "../../../services";
+import { toast } from "react-toastify";
 
 export const FeaturedProducts = () => {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     async function fetchProducts() {
-      const data = await getFeaturedList();
-      setProducts(data);
+      try {
+        const data = await getFeaturedList();
+        setProducts(data);
+      } catch (error) {
+        toast.error(error.message, {
+          closeButton: true,
+          position: "bottom-center",
+          autoClose: 5000,
+          closeOnClick: true
+        });
+      }
     }
     fetchProducts();
   }, [])
@@ -18,7 +28,7 @@ export const FeaturedProducts = () => {
       <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">Featured eBooks</h1>
       <div className="flex flex-wrap justify-center lg:flex-row">
         {products.map((product) => (
-          <ProductCard key={product.id} product={product}/>
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </section>
