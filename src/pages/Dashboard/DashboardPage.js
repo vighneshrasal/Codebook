03414b791ef7bea41511984559be688dc54f8f64
Vighnesh Rasal <vighneshrasal@gmail.com>
2024-12-09 12:38:24 +0530

    Update DashboardPage.js

--- src/pages/Dashboard/DashboardPage.js
+++ src/pages/Dashboard/DashboardPage.js
@@ -2,34 +2,44 @@ import { useEffect, useState } from "react";
 import { DashboardCard } from "./components/DashboardCard";
 import { DashboardEmpty } from "./components/DashboardEmpty";
 import { getUserOrders } from "../../services";
+import { toast } from 'react-toastify';
 
 export const DashboardPage = () => {
 
   const [orders, setOrders] = useState([]);
-    
+
   useEffect(() => {
     async function fetchOrders() {
-      const data = await getUserOrders();
-    setOrders(data);
+      try {
+        const data = await getUserOrders();
+        setOrders(data);
+      } catch (error) {
+        toast.error(error.message, {
+          closeButton: true,
+          position: "bottom-center",
+          autoClose: 5000,
+          closeOnClick: true
+        });
+      }
     }
     fetchOrders();
   }, []);
 
-    return (
-      <main>
-        <section>
-          <p className="text-2xl text-center font-semibold dark:text-slate-100 my-10 underline underline-offset-8">My Dashboard</p>
-        </section>
+  return (
+    <main>
+      <section>
+        <p className="text-2xl text-center font-semibold dark:text-slate-100 my-10 underline underline-offset-8">My Dashboard</p>
+      </section>
 
-        <section>
-            {orders.length && orders.map((order) => (
-                <DashboardCard key={order.id} order={order}/>
-            ))}
-        </section>
+      <section>
+        {orders.length && orders.map((order) => (
+          <DashboardCard key={order.id} order={order} />
+        ))}
+      </section>
 
-        <section>
-            {!orders.length && <DashboardEmpty/>}
-        </section>
-      </main>
-    )
-  }
+      <section>
+        {!orders.length && <DashboardEmpty />}
+      </section>
+    </main>
+  )
+}
